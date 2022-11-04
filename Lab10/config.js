83324dc914359b62d143cd247d3bf7c1ef8acbbe
Emile <eab359@cornell.edu>
2022-11-04 03:13:19 -0400

    Update config.js

--- Lab10/config.js
+++ Lab10/config.js
@@ -1,61 +1,214 @@
 var config = {
-    style: 'mapbox://styles/mapbox/outdoors-v11',
+    style: 'mapbox://styles/sedribenne/cl9bwby3n000314po3nbe0n1z',
     accessToken: 'pk.eyJ1Ijoic2VkcmliZW5uZSIsImEiOiJjbDlyNHNseDUwZDd4M3VxeHV6eGxzNHYwIn0.7l11nWXJKl6YqbifSZVLmQ',
-    showMarkers: true,
-    markerColor: '#3FB1CE',
+    showMarkers: false,
+    //markerColor: '#3FB1CE',
     //projection: 'equirectangular',
     //Read more about available projections here
     //https://docs.mapbox.com/mapbox-gl-js/example/projections/
-    inset: true,
+    inset: false,
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
-    title: 'The Title Text of this Story',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    byline: 'By a Digital Storyteller',
+    title: 'Flooding in the Future of New York',
+    subtitle: "An exploration into the projected impacts of flooding on New York City's public housing",
+    byline: 'By Emile Bensedrine, under the guidance of Dr. Wenfei Xu',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
+        { id: 'chapter1',
+            alignment: 'center',
+            description:'In late October of 2012, Hurricane Sandy made landfall in New York City, causing unprecedented damage to the then underprepared metropolis.',
+            location: {
+                center: [-73.99252, 40.65727],
+                zoom: 12.23,
+                pitch: 60,
+                bearing: 16.00,
+                curve: 1,
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+                onChapterEnter: [
+                 {
+                     layer: 'futurefloodplains-2050-100yr-6nepei',
+                     opacity: 0,
+                     duration: 600
+                 }
+                 ],
+                onChapterEnter: [
+                 {
+                     layer: 'sandy-inundation-zone-4abxur',
+                     opacity: 1,
+                     duration: 300
+                 }
+            ],
+        },
+
         {
-            id: 'slug-style-id',
+            id: 'chapter2',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
-            image: 'images/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            title: 'Extensive damage with long-lasting effects',
+            image: 'Sandy_damage.png',
+            description: "The extent of the damage covered large swaths of the city's low-lying coastal areas, many remaining without power for months after the storm surge.",
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: 0
+                center: [-73.93312, 40.59525],
+                zoom: 12.43,
+                pitch: 63.50,
+                bearing: -18.40,
+                speed: 2,
+                curve: 1,
+            },
+            mapAnimation: 'easeTo',
+            rotateAnimation: true,
+            callback: '',
+        },
+
+        {
+            id: 'chapter3',
+            alignment: 'left',
+            hidden: false,
+            title: 'Effects on Public Housing',
+            description: "1 of 5 New Yorkers lives the some 177,000 public housing units managed by the New York City Housing Authority around the city. Many NYCHA developments, which at the time had an estimated backlog of $17 billion, were significantly affected by the storm due to their locations.",
+            location: {
+                center: [-73.93105, 40.67213],
+                zoom: 10.95,
+                pitch: 42.00,
+                bearing: 12.00,
+                speed: 2,
+                curve: 1,
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [
+                 {
+                     layer: 'sandy-inundation-zone-4abxur',
+                     opacity: 1,
+                     duration: 5000
+                 }
+            ],            
+            onChapterEnter: [
+                 {
+                     layer: 'pluto-hous-type-master-featu-7b5u7f',
+                     opacity: 1,
+                     duration: 600
+                 }
+            ],
+        },
+        {
+            id: 'chapter5',
+            alignment: 'left',
+            hidden: false,
+            title: 'The Red Hook Houses',
+            image: 'Red_Hook_Houses.png',
+            description: 'The neighborhood of Red Hook, in Brooklyn, is home to the Red Hook Houses, a NYCHA development almost completely inundated by Hurricane Sandy.',
+            location: {
+                center: [-74.00595, 40.67524],
+                zoom: 15.95,
+                pitch: 62.50,
+                bearing: -93.60,
+                speed: 2,
+                curve: 1,
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1,
-                //     duration: 5000
-                // }
+                 {
+                     layer: 'pluto-hous-type-master-featu-7b5u7f',
+                     opacity: 1,
+                     duration: 600
+                 }
             ],
+            onChapterEnter: [
+                 {
+                     layer: 'sandy-inundation-zone-4abxur',
+                     opacity: .5,
+                     duration: 600
+                 }
+            ],
+        
             onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
-            ]
+                 {
+                     layer: 'sandy-inundation-zone-4abxur',
+                     opacity: 0,
+                     duration: 600
+                 }
+            ],
         },
         {
-            id: 'second-identifier',
+            id: 'chapter4',
             alignment: 'right',
             hidden: false,
-            title: 'Second Title',
-            image: 'images/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'The Red Hook Houses are expected to be partially encompassed by 100-year flooding in the year 2050...',
             location: {
-                center: [-77.020636, 38.886900],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: -43.2,
+                center: [-74.00595, 40.67524],
+                zoom: 15.95,
+                pitch: 62.50,
+                bearing: -93.60
+                },
+                mapAnimation: 'flyTo',
+                speed: 2, // make the flying slow
+                curve: 1,
+                rotateAnimation: false,
+                callback: '',
+                
+                onChapterEnter: [
+                 {
+                     layer: 'pluto-hous-type-master-featu-7b5u7f',
+                     opacity: .5,
+                     duration: 300
+                 }
+                ],
+                onChapterEnter: [
+                 {
+                     layer: 'futurefloodplains-2050-100yr-6nepei',
+                     opacity: 1,
+                     duration: 600
+                 }
+                 ],
+                onChapterExit: [
+                 {
+                     layer: 'futurefloodplains-2050-100yr-6nepei',
+                     opacity: 0,
+                     duration: 600
+                 }
+                 ]
+        },
+                {
+            id: 'chapter4pt2',
+            alignment: 'right',
+            hidden: false,
+            description: 'And completely inundated in the year 2100.',
+            location: {
+                center: [-74.00595, 40.67524],
+                zoom: 15.95,
+                pitch: 62.50,
+                bearing: -93.60},
+                // mapAnimation: 'flyTo',
+                // speed: 2, // make the flying slow
+                // curve: 1,
+                rotateAnimation: false,
+                callback: '',
+                onChapterEnter: [
+                 {
+                     layer: 'futurefloodplains-2100-100yr-cqwfrw',
+                     opacity: 1,
+                     duration: 600
+                 }
+                ],
+                
+            },
+            {
+            id: 'chapter5',
+            alignment: 'center',
+            hidden: false,
+            title: 'Looking Forward',
+            description: 'The flood resiliency of NYCHA housing developments such as the Red Hook Houses, thus, will be of increasing concern especially as the public housing maintenance backlog continues.',
+            location: {
+                center: [-74.00595, 40.67524],
+                zoom: 15.95,
+                pitch: 62.50,
+                bearing: -93.60
                 // flyTo additional controls-
                 // These options control the flight curve, making it move
                 // slowly and zoom out almost completely before starting
@@ -69,43 +222,7 @@ var config = {
             onChapterEnter: [],
             onChapterExit: []
         },
-        {
-            id: 'third-identifier',
-            alignment: 'left',
-            hidden: false,
-            title: 'Third Title',
-            image: 'images/source.png',
-            description: 'Copy these sections to add to your story.',
-            location: {
-                center: [6.15116, 46.20595],
-                zoom: 12.52,
-                pitch: 8.01,
-                bearing: 0.00
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'fourth-chapter',
-            alignment: 'fully',
-            hidden: false,
-            title: 'Third Title',
-            image: 'images/source.png',
-            description: 'Copy these sections to add to your story.',
-            location: {
-                center: [-58.54195, -34.71600],
-                zoom: 4,
-                pitch: 0,
-                bearing: 0
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        }
-    ]
+]
+
+    
 };
