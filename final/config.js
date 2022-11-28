9d81accc182a0f53e210497de9314deb4f55af7b
Emile <eab359@cornell.edu>
2022-11-28 16:23:34 -0500

    Update config.js

--- final/config.js
+++ final/config.js
@@ -6,44 +6,176 @@ var config = {
     //projection: 'equirectangular',
     //Read more about available projections here
     //https://docs.mapbox.com/mapbox-gl-js/example/projections/
-    inset: true,
+    inset: false,
     theme: 'light',
     use3dTerrain: true, //set true for enabling 3D maps.
     title: 'Hasidim on Hudson',
     subtitle: 'A Narrative of Hasidic development in Rockland County',
-    byline: 'By Emile Bensedrine and Kellen Cooks',
-    para1:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur justo eros, ac eleifend nibh porta eget. Donec orci odio, varius nec urna sit amet, vehicula vulputate tellus. Fusce non iaculis felis. Integer ut neque id lectus ultrices suscipit. Vivamus quis condimentum ipsum. Ut at tempor velit. Maecenas commodo eu sem et interdum. Nam efficitur nisi sed enim interdum pharetra. Vestibulum turpis nisl, dignissim at dapibus at, laoreet at enim.',
-    para2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur justo eros, ac eleifend nibh porta eget. Donec orci odio, varius nec urna sit amet, vehicula vulputate tellus. Fusce non iaculis felis. Integer ut neque id lectus ultrices suscipit. Vivamus quis condimentum ipsum. Ut at tempor velit. Maecenas commodo eu sem et interdum. Nam efficitur nisi sed enim interdum pharetra. Vestibulum turpis nisl, dignissim at dapibus at, laoreet at enim.',
+    authors: 'By Emile Bensedrine and Kellen Cooks',
+    para1:'The Hudson Valley, which stretches from the northern boundary of New York City to Albany, contains numerous diverse communities with equally diverse development trajectories. With its proximity to New York, it has long attracted tourists. Since its connection to the Erie Canal, the Hudson Valley became a hub of industry and manufacturing. Areas of the Lower Hudson Valley in particular have seen the development of affluent suburban communities. Over the past few decades, the Hudson Valley like much of the nation has seen significant deindustrialization. Towns in decline have thus seen two types of revitalization, one from the influx of largely Latino immigrants, and the other through amenity development driven by luxury real estate investment targeting more affluent newcomers.',
+    para2:'Another, often unmentioned, type of development has also played a significant role in transforming the Hudson Valley, that caused by the influx of Hasidic Communities into established suburban neighborhoods. Hasidic development in the Lower Hudson Valley represents a unique and separate development pattern that has led to significant inter-communal tensions ranging from zoning conflicts, to displacement, to the divestment from the local school district. The following content is an exploration into the story of Hasidic Development in Rockland County, its scale, and its impacts. The intention of the data is not to demonize any group, but rather to contextualize and shed light on a little-understood phenomenon riddled with controversy.',
     footer: 'Image source: https://www.theatlantic.com/photo/2012/11/hurricane-sandy-the-aftermath/100397/ Video source: Dr. Wenfei Xu <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     
+    chapters: [        
+    	{
+            id: 'first_chapter',
+            alignment: 'right',
+            hidden: false,
+            title: 'Geography',
+            description:'Rockland county lies at the southern end on the west bank of the Hudson River Valley. Throughout the mid-to-late 20th century, the county became increasingly suburbanized with the influx of white city residents moving to the area.',
+            location: {
+			center: [-73.92340, 41.10497],
+			zoom: 7.5,
+			pitch: 0.00,
+			bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            speed: .1, // make the flying slow
+                        onChapterEnter: [
+                {
+                    layer: '1960-race-637khu',
+                    opacity: 0
+                },
+                {
+                    layer: 'New Square 61',
+                    opacity: 0
+                },
+                {
+                    layer: 'Pomona 1967',
+                    opacity: 0
+                },
+                {
+                    layer: 'map-labels-60s',
+                    opacity: 0
+                },
+                {
+                    layer: 'map-labels (1)',
+                    opacity: 0
+                },                
+                {
+                    layer: 'map-labels-90s',
+                    opacity: 0
+                },                               
+                {
+                    layer: 'Wesley Hills 83',
+                    opacity: 0
+                },
+                {
+                    layer: 'New Hempstead 84',
+                    opacity: 0
+                },
+                {
+                    layer: 'Chestnut Ridge 86',
+                    opacity: 0
+                },
+                {
+                    layer: 'Airmont 91',
+                    opacity: 0
+                },
+                {
+                    layer: 'Montebello 86',
+                    opacity: 0
+                },
+                {
+                    layer: 'kaser 91',
+                    opacity: 0
+                },                                                                                                                                  
+            ],
 
-
-
-
-    chapters: [
+        },
+            {
+		    id: 'interlude',
+		    alignment: 'center',
+		    hidden: false,
+		    description: '"There are two reasons villages get formed in Rockland, One is to keep the Hasidim out and the other is to keep the Hasidim in."',
+		    location: {
+                center: [-73.99420, 41.12572],
+				zoom: 10.20,
+				pitch: 0.00,
+				bearing: 0.00
+		    }
+		},
         {
             id: 'chapter60s',
-            alignment: 'left',
+            alignment: 'right',
             hidden: false,
             title: '1960s',
-            // image: 'hedgehog-in-the-fog.3_f.jpg',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            description:'Around the 1960s, a Jewish community of Skver Hasidic dynasty, many Holocaust survivors, moved to Rockland county from Brooklyn after purchasing a 160-acre dairy farm. The Skverer Hasidim were some of the first of many groups of Hasidim to leave New York City due to fears of assimilation. The group formed New Square Specifically so that they could permit denser housing to be built.',
             location: {
-                center: [-73.95988, 41.16896],
-				zoom: 10.20,
+                center: [-73.95988, 41.16000],
+				zoom: 10.3,
 				pitch: 0.00,
 				bearing: 0.00
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
+            speed: .1, // make the flying slow
             onChapterEnter: [
                 {
                     layer: '1960-race-637khu',
                     opacity: 1
-                }                                                                                                                  
+                },
+                {
+                    layer: 'New Square 61',
+                    opacity: 1
+                },
+                {
+                    layer: 'Pomona 1967',
+                    opacity: 1
+                },
+                {
+                    layer: 'map-labels-60s',
+                    opacity: 1
+                },
+                {
+                    layer: 'map-labels (1)',
+                    opacity: 0
+                },                
+                {
+                    layer: 'map-labels-90s',
+                    opacity: 0
+                },                               
+                {
+                    layer: 'Wesley Hills 83',
+                    opacity: 0
+                },
+                {
+                    layer: 'New Hempstead 84',
+                    opacity: 0
+                },
+                {
+                    layer: 'Chestnut Ridge 86',
+                    opacity: 0
+                },
+                {
+                    layer: 'Airmont 91',
+                    opacity: 0
+                },
+                {
+                    layer: 'Montebello 86',
+                    opacity: 0
+                },
+                {
+                    layer: 'kaser 91',
+                    opacity: 0
+                },                                                                                                                                  
             ],
             onChapterExit: [
+                {
+                    layer: 'New Square 61',
+                    opacity: 0
+                },
+                {
+                    layer: 'Pomona 1967',
+                    opacity: 0
+                },
+                {
+                    layer: 'map-labels-60s',
+                    opacity: 0
+                },                
                 {
                     layer: '1960-race-637khu',
                     opacity: 0
@@ -61,10 +193,10 @@ var config = {
             hidden: false,
             title: '1970s',
             image: 'images/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'The years following, Hasidic communities continued to grow in Rockland and neighboring Orange counties as more Hasidim moved into the region. Different Hasidic groups often settled in different areas, with the Vizhnitz Dynasty beggining to settle in the neighborhood of Monsey.',
             location: {
-                center: [-73.95988, 41.16896],
-				zoom: 10.20,
+                center: [-73.95988, 41.16000],
+				zoom: 10.30,
 				pitch: 0.00,
 				bearing: 0.00
             },
@@ -78,6 +210,22 @@ var config = {
                 {
                 	layer: '1980-race-09ndgz',
                     opacity: 1,
+                },               
+                {
+                    layer: 'Wesley Hills 83',
+                    opacity: 1
+                },
+                {
+                    layer: 'New Hempstead 84',
+                    opacity: 1
+                },
+                {
+                    layer: 'Chestnut Ridge 86',
+                    opacity: 1
+                },
+                {
+                    layer: 'Montebello 86',
+                    opacity: 1
                 },
                 ]
         },
@@ -87,15 +235,19 @@ var config = {
             hidden: false,
             title: '1980s',
             image: 'images/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: '.',
             location: {
-                center: [-73.95988, 41.16896],
-				zoom: 10.20,
+                center: [-73.95988, 41.16000],
+				zoom: 10.30,
 				pitch: 0.00,
 				bearing: 0.00
             },
             callback: '',
-            onChapterEnter: [],
+            onChapterEnter: [
+                    {
+                 	layer: 'map-labels (1)',
+                    opacity: 1
+                }],
             onChapterExit: [            	{
                     layer: '1980-race-09ndgz',
                     opacity: 0
@@ -103,7 +255,44 @@ var config = {
                 {
                 	layer: '1990-race-940279',
                     opacity: 1,
-                },]
+                },
+                                {
+                	layer: '1980-race-09ndgz',
+                    opacity: 0,
+                },               
+                {
+                    layer: 'Wesley Hills 83',
+                    opacity: 0
+                },
+                {
+                    layer: 'New Hempstead 84',
+                    opacity: 0
+                },
+                {
+                    layer: 'Chestnut Ridge 86',
+                    opacity: 0
+                },
+                {
+                    layer: 'Montebello 86',
+                    opacity: 0
+                },
+                {
+                    layer: 'Airmont 91',
+                    opacity: 1
+                },
+                {
+                    layer: 'kaser 91',
+                    opacity: 1
+                },
+                {
+                 	layer: 'map-labels (1)',
+                    opacity: 0
+                },
+                {
+                	layer: 'map-labels-90s',
+                	opacity: 1
+                }
+                ]
         },
         {
             id: 'chapter90s',
@@ -111,10 +300,10 @@ var config = {
             hidden: false,
             title: '1990s',
             image: 'images/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'The last of the anti-orthodox reactionary villages to be founded was Airmont, founded to have "strong zoning" that would keep the "character of the neighborhood" applying zoning that was unconducive to the Hasidic lifestyle.',
             location: {
-                center: [-73.95988, 41.16896],
-				zoom: 10.20,
+                center: [-73.95988, 41.16000],
+				zoom: 10.30,
 				pitch: 0.00,
 				bearing: 0.00
             },
@@ -137,8 +326,8 @@ var config = {
             image: 'images/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-73.95988, 41.16896],
-				zoom: 10.20,
+                center: [-73.95988, 41.16000],
+				zoom: 10.30,
 				pitch: 0.00,
 				bearing: 0.00
             },
@@ -161,8 +350,8 @@ var config = {
             image: 'images/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-73.95988, 41.16896],
-				zoom: 10.20,
+                center: [-73.95988, 41.16000],
+				zoom: 10.30,
 				pitch: 0.00,
 				bearing: 0.00
 				},
@@ -185,8 +374,8 @@ var config = {
             image: 'images/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-73.95988, 41.16896],
-				zoom: 10.20,
+                center: [-73.95988, 41.16000],
+				zoom: 10.30,
 				pitch: 0.00,
 				bearing: 0.00
             },
@@ -197,36 +386,49 @@ var config = {
                     opacity: 0
                 },]
         },
-  //       {
-		//     id: 'interlude',
-		//     alignment: 'center',
-		//     hidden: false,
-		//     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultricies tempor purus, eget tempus sem bibendum vel. Maecenas at justo non felis accumsan condimentum vitae et leo. Sed sodales nec risus maximus congue. Ut quis tincidunt leo. Pellentesque sapien nisl, congue tempus mauris in, pulvinar mattis turpis. Nullam sit amet justo egestas, laoreet mauris ut, pulvinar ipsum. Pellentesque vehicula imperdiet iaculis. Nam in tellus metus. Donec a est non orci efficitur ornare id mattis eros. Aliquam vel facilisis ligula. Nullam tincidunt, sapien vitae bibendum consequat, nibh eros eleifend ligula, et aliquam enim justo eget felis. Sed nec vehicula metus. Pellentesque rutrum dui quis egestas hendrerit. Nullam auctor et tortor sit amet congue.',
-		//     video: 'Images/902-1_902-2364-PD2_preview.mp4',
-		//     location: {
-  //               center: [-73.99420, 41.12572],
-		// 		zoom: 10.20,
-		// 		pitch: 0.00,
-		// 		bearing: 0.00
-		//     }
-		// },
         {
-            id: 'third-chapter',
+            id: 'Eastern_europe',
             alignment: 'left',
             hidden: false,
-            title: 'Third Title',
+            title: 'Origins of the Hasidim',
             description: 'Copy these sections to add to your story.',
 
             location: {
-              center: [-73.95988, 41.16896],
-              zoom: 15.84,
-              pitch: 56,
-              bearing: 260.44,
-              speed: .5, // make the flying slow
+              	center: [22.87423, 47.76870],
+				zoom: 6.44,
+				pitch: 56.00,
+				bearing: 11.20,
+              speed: 1, // make the flying slow
               curve: 1, // change the speed at which it zooms out
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: false,
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [
+                {
+                    layer: 'housing',
+                    opacity: 0,
+                    duration: 10
+                }],
+            onChapterExit: []
+        },
+        {
+            id: 'Eastern_europe',
+            alignment: 'left',
+            hidden: false,
+            title: 'Origins of the Hasidim',
+            description: 'Copy these sections to add to your story.',
+
+            location: {
+				center: [-73.96303, 40.64629],
+				zoom: 11.76,
+				pitch: 44.13,
+				bearing: -16.84,
+              speed: 1, // make the flying slow
+              curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
             callback: '',
             onChapterEnter: [
                 {
